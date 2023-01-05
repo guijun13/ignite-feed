605a8a2c28e26cf25c01db1a0e77116fa778acb0
guijun13 <guilhermejun13.grillo@gmail.com>
2023-01-05 11:44:43 -0300

    styling the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Header } from "./componentes/Header"
+import { Header } from "./components/Header"
+
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
 
   return (
-    <div className="App">
+    <div >
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        {/* <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main> */}
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1536882240095-0379873feb4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50https://images.unsplash.com/photo-1536882240095-0379873feb4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/guijun13.png" />
+        <strong>Guilherme Jun</strong>
+        <span>Front-End developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
