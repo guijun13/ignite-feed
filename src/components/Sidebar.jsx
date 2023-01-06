45c14055a97908306f2577a97be6ba8355022f3e
guijun13 <guilhermejun13.grillo@gmail.com>
2023-01-05 22:24:21 -0300

    refactoring the avatars

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,7 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1536882240095-0379873feb4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50https://images.unsplash.com/photo-1536882240095-0379873feb4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/guijun13.png" />
+      <Avatar src="https://github.com/guijun13.png" />
         <strong>Guilherme Jun</strong>
         <span>Front-End developer</span>
       </div>
