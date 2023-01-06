71de7644b82af0b3ddb7be4ec86f7e80e35fb341
guijun13 <guilhermejun13.grillo@gmail.com>
2023-01-05 22:06:02 -0300

    add comment component

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,6 @@ export function App() {
             author="Diego Fernandes"
             content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
           />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
         </main>
       </div>
     </div>
