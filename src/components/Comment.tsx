82cd5865d9f1c051b3dcd732043989b5fefa0b37
guijun13 <guilhermejun13.grillo@gmail.com>
2023-01-10 01:25:16 -0300

    types in avatar

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -24,7 +24,7 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/guijun13.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
